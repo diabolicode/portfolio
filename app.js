c6c0bfbcee6f9f97312aeda82349b76f403bc1c3
Priya Mohanram <priyamo@microsoft.com>
2017-02-03 14:16:33 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = angular.module("website", []);
+app.controller("portfolioController", function(){
+    this.portfolio = myPortfolio;
+});
+
+var myPortfolio = [
+    {
+        id: 0,
+        title: "Software development",
+        img: "img/portfolio/cabin.png",
+        description: "Created an extension in VS Code to help content developers iterate faster.",
+        client: "Microsoft",
+        date: "August 2016",
+        tags: ["Software Development", "Typescript"]
+    },
+    {
+        id: 1,
+        title: "Music",
+        img: "img/portfolio/cake.png",
+        description: "Active on SoundCloud.com under LadyChatterley",
+        client: "SoundCloud",
+        date: "July 2015",
+        tags: ["Music"]
+    },
+    {
+        id: 2,
+        title: "Artist",
+        img: "img/portfolio/circus.png",
+        description: "Created many paintings",
+        client: "Priya",
+        date: "April 2015",
+        tags: ["Art"]
+    }
+];
